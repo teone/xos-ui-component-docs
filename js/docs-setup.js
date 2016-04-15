4c0181be31259aaaaf71f0d6df6274e3ab5487fc
Matteo Scandolo <teo@onlab.us>
2016-04-15 15:56:35 -0700

    Starting point

---
+++ js/docs-setup.js
@@ -0,0 +1,229 @@
+NG_DOCS={
+  "sections": {
+    "module": "Module Documentation",
+    "rest-api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "module",
+      "id": "xos.helpers",
+      "shortName": "xos.helpers",
+      "type": "overview",
+      "moduleName": "xos.helpers",
+      "shortDescription": "this is the module that group all the helpers service and components for XOS",
+      "keywords": "components group helpers module overview service xos"
+    },
+    {
+      "section": "module",
+      "id": "xos.helpers.NoHyperlinks",
+      "shortName": "xos.helpers.NoHyperlinks",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "This factory is automatically loaded trough xos.helpers and will add an $http interceptor that will add ?no_hyperlinks=1 to your api request, that is required by django",
+      "keywords": "$http add api automatically django factory helpers interceptor loaded module nohyperlinks request required service trough xos"
+    },
+    {
+      "section": "module",
+      "id": "xos.helpers.SetCSRFToken",
+      "shortName": "xos.helpers.SetCSRFToken",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "This factory is automatically loaded trough xos.helpers and will add an $http interceptor that will the CSRF-Token to your request headers",
+      "keywords": "$http add automatically csrf-token factory headers helpers interceptor loaded module request service setcsrftoken trough xos"
+    },
+    {
+      "section": "module",
+      "id": "xos.uiComponents",
+      "shortName": "xos.uiComponents",
+      "type": "overview",
+      "moduleName": "xos.uiComponents",
+      "shortDescription": "A collection of UI components useful for Dashboard development",
+      "keywords": "collection components dashboard development module overview ui uicomponents xos"
+    },
+    {
+      "section": "module",
+      "id": "xos.uiComponents.directive:xosAlert",
+      "shortName": "xosAlert",
+      "type": "directive",
+      "moduleName": "xos.uiComponents",
+      "shortDescription": "The xos-alert directive",
+      "keywords": "alert angular autohide automatically binding btn btn-block btn-default button class close closebtn col-sm-4 col-sm-8 config config1 config2 config3 config4 config5 configuration controller danger default directive displayed false function hide html info js message module ng-click ng-controller ng-show object row sample samplealert1 samplealert2 samplectrl samplectrl1 script success true type uicomponents vm warning xos xos-alert"
+    },
+    {
+      "section": "module",
+      "id": "xos.uiComponents.directive:xosPagination",
+      "shortName": "xosPagination",
+      "type": "directive",
+      "moduleName": "xos.uiComponents",
+      "shortDescription": "The xos-table directive",
+      "keywords": "angular callback change collection console controller directive elements function html js log module ng-controller number page-size pagesize samplectrl1 samplepagination script total total-elements totalelements triggered uicomponents vm xos xos-table"
+    },
+    {
+      "section": "module",
+      "id": "xos.uiComponents.directive:xosTable",
+      "shortName": "xosTable",
+      "type": "directive",
+      "moduleName": "xos.uiComponents",
+      "shortDescription": "The xos-table directive",
+      "keywords": "action actions add angular array arrows bootstraps callback cb clarkson classes color column columns component config configuration console controller data default defined delete directive doe feeded fereydoun field filter full fulltext function gili glyphicon html human icon icons john js label lastname log lucky model module ng-controller object order ordering pagesize pagination prop property read readable receive red refers remove rendered samplectrl1 samplectrl2 samplectrl3 sampletable1 sampletable2 sampletable3 script spalding table table-bordered table-condensed table-striped tate title true uicomponents vm xos xos-table"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.ONOS-App-Collection",
+      "shortName": "xos.helpers.ONOS-App-Collection",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/onos/app/",
+      "keywords": "angular fetch helpers onos-app-collection resource rest-api service xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.ONOS-Services-Collection",
+      "shortName": "xos.helpers.ONOS-Services-Collection",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/service/onos/",
+      "keywords": "angular fetch helpers onos-services-collection resource rest-api service xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Subscriber-features",
+      "shortName": "xos.helpers.Subscriber-features",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/cord/subscriber/:subscriber_id/features/",
+      "keywords": "angular fetch helpers resource rest-api service subscriber-features xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Subscriber-features-cdn",
+      "shortName": "xos.helpers.Subscriber-features-cdn",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/cord/subscriber/:subscriber_id/features/cdn/",
+      "keywords": "angular fetch helpers resource rest-api service subscriber-features-cdn xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Subscriber-features-downlink_speed",
+      "shortName": "xos.helpers.Subscriber-features-downlink_speed",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/cord/subscriber/:subscriber_id/features/downlink_speed/",
+      "keywords": "angular fetch helpers resource rest-api service subscriber-features-downlink_speed xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Subscriber-features-status",
+      "shortName": "xos.helpers.Subscriber-features-status",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/cord/subscriber/:subscriber_id/features/status/",
+      "keywords": "angular fetch helpers resource rest-api service subscriber-features-status xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Subscriber-features-uplink_speed",
+      "shortName": "xos.helpers.Subscriber-features-uplink_speed",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/cord/subscriber/:subscriber_id/features/uplink_speed/",
+      "keywords": "angular fetch helpers resource rest-api service subscriber-features-uplink_speed xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Subscriber-features-uverse",
+      "shortName": "xos.helpers.Subscriber-features-uverse",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/cord/subscriber/:subscriber_id/features/uverse/",
+      "keywords": "angular fetch helpers resource rest-api service subscriber-features-uverse xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Subscribers",
+      "shortName": "xos.helpers.Subscribers",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/cord/subscriber/:subscriber_id/",
+      "keywords": "angular fetch helpers resource rest-api service subscribers xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Truckroll-Collection",
+      "shortName": "xos.helpers.Truckroll-Collection",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/truckroll/:truckroll_id/",
+      "keywords": "angular fetch helpers resource rest-api service truckroll-collection xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Users",
+      "shortName": "xos.helpers.Users",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/core/users/",
+      "keywords": "angular fetch helpers resource rest-api service users xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.vOLT-Collection",
+      "shortName": "xos.helpers.vOLT-Collection",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/tenant/cord/volt/:volt_id/",
+      "keywords": "angular fetch helpers resource rest-api service volt-collection xos"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.vSG-Collection",
+      "shortName": "xos.helpers.vSG-Collection",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/service/vsg/",
+      "keywords": "angular fetch helpers resource rest-api service vsg-collection xos"
+    }
+  ],
+  "apis": {
+    "module": true,
+    "rest-api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/module",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js",
+      "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.min.js",
+      "../xos/core/xoslib/static/js/vendor/ngXosHelpers.js"
+    ],
+    "styles": [
+      "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    ],
+    "title": "XOS Helpers documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/module",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js",
+    "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.min.js",
+    "../xos/core/xoslib/static/js/vendor/ngXosHelpers.js"
+  ]
+};
