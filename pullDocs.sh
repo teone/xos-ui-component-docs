1c985449b1713589d3b05675b21826bba2d425d9
Matteo Scandolo <teo@onlab.us>
2016-04-28 13:30:41 -0700

    updated docs

--- pullDocs.sh
+++ pullDocs.sh
@@ -2,8 +2,8 @@ cp -R ../xos/views/ngXosLib/docs/ .
 cp ../xos/xos/core/xoslib/static/js/vendor/ngXosHelpers.js ./xos/core/xoslib/static/js/vendor/ngXosHelpers.js
 cp ../xos/xos/core/static/xosNgLib.css  ./xos/core/static/xosNgLib.css
 
-sed s%../xos/core/xoslib/static/js/vendor/ngXosHelpers.js%xos/core/xoslib/static/js/vendor/ngXosHelpers.js% <index.html >index.html
-sed s%../xos/core/static/xosNgLib.css%xos/core/static/xosNgLib.css% <index.html >index.html
+#sed s%../xos/core/xoslib/static/js/vendor/ngXosHelpers.js%xos/core/xoslib/static/js/vendor/ngXosHelpers.js% <index.html >index.html
+#sed s%../xos/core/static/xosNgLib.css%xos/core/static/xosNgLib.css% <index.html >index.html
 
 git status
 
