f1d0331e0c2563c81d4f789c03c40c027debd517
Matteo Scandolo <teo@onlab.us>
2016-04-28 13:15:34 -0700

    Updated docs

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -15,6 +15,15 @@ NG_DOCS={
     },
     {
       "section": "module",
+      "id": "xos.helpers.LabelFormatter",
+      "shortName": "xos.helpers.LabelFormatter",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "This factory define a set of helper function to format label started from an object property",
+      "keywords": "define factory format function helper helpers label labelformatter module object property service set started xos"
+    },
+    {
+      "section": "module",
       "id": "xos.helpers.NoHyperlinks",
       "shortName": "xos.helpers.NoHyperlinks",
       "type": "service",
@@ -47,7 +56,16 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "xos.uiComponents",
       "shortDescription": "The xos-alert directive",
-      "keywords": "alert angular autohide automatically binding btn btn-block btn-default button class close closebtn col-sm-4 col-sm-8 config config1 config2 config3 config4 config5 configuration controller danger default directive displayed false function hide html info js message module ng-click ng-controller ng-show object row sample samplealert1 samplealert2 samplectrl samplectrl1 script success true type uicomponents vm warning xos xos-alert"
+      "keywords": "alert angular animations autohide automatically binding btn btn-block btn-default button class close closebtn col-sm-4 col-sm-8 config config1 config2 config3 config4 config5 configuration controller danger default directive displayed false function hide html info js message module ng-click ng-controller ng-show nganimate object row sample samplealert1 samplealert2 samplectrl samplectrl1 script success true type uicomponents vm warning xos xos-alert"
+    },
+    {
+      "section": "module",
+      "id": "xos.uiComponents.directive:xosForm",
+      "shortName": "xosForm",
+      "type": "directive",
+      "moduleName": "xos.uiComponents",
+      "shortDescription": "The xos-form directive",
+      "keywords": "$window actions active angular birthdate bootstraps buttons callback cb class concatenated config configuration console controller created define deleted directive doe email exclude factory first_name form formname function glyphicon html icon jhon js label last_login last_name log model module ng-controller ng-model object password provide receive refers return samplectrl sampleform save script skipped success true uicomponents updated validators values vm xos xos-form"
     },
     {
       "section": "module",
@@ -60,12 +78,39 @@ NG_DOCS={
     },
     {
       "section": "module",
+      "id": "xos.uiComponents.directive:xosSmartTable",
+      "shortName": "xosSmartTable",
+      "type": "directive",
+      "moduleName": "xos.uiComponents",
+      "shortDescription": "The xos-table directive",
+      "keywords": "$httpbackend $resource $window angular angularjs array component composed config configuration controller count data datas directive documentation doe example factory fields fromjson function headers hiddenfields html jhon js match module ng-controller ngmocke2e ngresource org params paramsurl parseint printed purpose push regexp remove resource respond return samplectrl sampleresource samplesmarttable script service shouldn start surname uicomponents undefined url users vm whendelete whenget whenpost xos xos-table"
+    },
+    {
+      "section": "module",
       "id": "xos.uiComponents.directive:xosTable",
       "shortName": "xosTable",
       "type": "directive",
       "moduleName": "xos.uiComponents",
       "shortDescription": "The xos-table directive",
-      "keywords": "action actions add angular array arrows bootstraps callback cb clarkson classes color column columns component config configuration console controller data default defined delete directive doe feeded fereydoun field filter full fulltext function gili glyphicon html human icon icons john js label lastname log lucky model module ng-controller object order ordering pagesize pagination prop property read readable receive red refers remove rendered samplectrl1 samplectrl2 samplectrl3 sampletable1 sampletable2 sampletable3 script spalding table table-bordered table-condensed table-striped tate title true uicomponents vm xos xos-table"
+      "keywords": "action actions add angular animations array arrows bootstraps callback cb clarkson classes color column columns component config configuration console controller data default defined delete directive doe feeded fereydoun field filter full fulltext function gili glyphicon html human icon icons john js label lastname log lucky model module ng-controller nganimate object order ordering pagesize pagination prop property read readable receive red refers remove rendered samplectrl1 samplectrl2 samplectrl3 sampletable1 sampletable2 sampletable3 script spalding table table-bordered table-condensed table-striped tate title true uicomponents vm xos xos-table"
+    },
+    {
+      "section": "module",
+      "id": "xos.uiComponents.directive:xosValidation",
+      "shortName": "xosValidation",
+      "type": "directive",
+      "moduleName": "xos.uiComponents",
+      "shortDescription": "The xos-validation directive",
+      "keywords": "angular btn btn-default btn-success class col-xs-12 col-xs-2 controller directive email error errors false function html js length max maxlength min minlength module ng-class ng-click ng-controller object required row samplectrl samplevalidation script type uicomponents vm xos xos-validation"
+    },
+    {
+      "section": "rest-api",
+      "id": "xos.helpers.Instances",
+      "shortName": "xos.helpers.Instances",
+      "type": "service",
+      "moduleName": "xos.helpers",
+      "shortDescription": "Angular resource to fetch /api/core/instances/",
+      "keywords": "angular fetch helpers instances resource rest-api service xos"
     },
     {
       "section": "rest-api",
@@ -196,12 +241,17 @@ NG_DOCS={
       "js/angular.min.js",
       "js/angular-animate.min.js",
       "js/marked.js",
-      "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js",
-      "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-animate.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-cookies.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-resource.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-mocks.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.11.2/lodash.js",
       "../xos/core/xoslib/static/js/vendor/ngXosHelpers.js"
     ],
     "styles": [
-      "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+      "../xos/core/static/xosNgLib.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.css"
     ],
     "title": "XOS Helpers documentation",
     "html5Mode": false,
@@ -222,8 +272,12 @@ NG_DOCS={
     "js/angular.min.js",
     "js/angular-animate.min.js",
     "js/marked.js",
-    "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js",
-    "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-animate.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-cookies.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-resource.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-mocks.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.11.2/lodash.js",
     "../xos/core/xoslib/static/js/vendor/ngXosHelpers.js"
   ]
 };
